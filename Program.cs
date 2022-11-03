d623ff04fbe006a2cc28f7e916eee61c7f8cf3bd
AlekseiKing <kingulya@mail.ru>
2022-11-03 14:10:07 +0300

    Working with the Method FillArray

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
 ﻿
 
+string[] FillArray()
+{
+    int n;
+    Console.Write("Введите число элементов массива: ");
+    int.TryParse(Console.ReadLine()!, out n);
+    string[] _array = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент: ");
+        _array[i] = Console.ReadLine()!;
+    }
+    return _array;
+}
 
+
+
+void PrintArray(string[] _array)
+{
+    foreach (var item in _array)
+    {
+        Console.Write($"{item} ");
+    }
+}
 
 
